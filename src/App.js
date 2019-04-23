91fe97128a1d52b7440e46118996017ee5b51e8f
Colton Freeze <freezecolton@gmail.com>
2019-04-23 14:10:48 -0600

    s

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import './index.css';
+import TopicBrowser from './components/TopicBrowser/TopicBrowser'
 
 class App extends Component {
   render() {
-    
+    return (
+      <TopicBrowser />
+    )
   }
 }
 
 export default App;
+
+{/* <div>
+<h4></h4>
+<input></input>
+<button></button>
+<span></span>
+<span></span>
+</div> */}
